38c398da21b56fe33889ced0168e5d4d3c829017
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2018-05-12 19:27:24 +0200

    Merge branches 'aga' and 'igi' + homescreen

---
+++ constants/icons.js
@@ -0,0 +1,5 @@
+import finger from '../assets/images/finger-print.png';
+import flower from '../assets/images/flower.png';
+import bowtie from '../assets/images/bowtie.png';
+
+export default {finger,flower, bowtie};
